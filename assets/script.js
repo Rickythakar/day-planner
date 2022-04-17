7fb42557de30cb5d4a154ee1ec1f0b55304abc35
Ricky Thakar <rickythakar@gmail.com>
2022-04-17 12:55:04 -0400

    added jquery variables and commented out javascript variables to compare the two

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,49 @@
 // variable to get current date and time
 var now = moment();
 console.log(now);
-var date = moment().format("dddd, MMMM Do YYYY, h:mm:ss a"); 
-var currentDay = document.getElementById("currentDay");
-currentDay.innerText = date;
+var date = moment().format("dddd, MMMM Do YYYY, h:mm a");
+
+//var currentDay = document.getElementById("currentDay");
+var $currentDay = $("#currentDay")
 
 
-var timeBlockElements = document.querySelectorAll(".time-block")
-console.log(timeBlockElements)
+// currentDay.innerText = date;
+$currentDay.text(date)
 
-var setTimeClass = function(arrayOfTimeBlocks) {
-    for (let index = 0; index < arrayOfTimeBlocks.length; index++) {
-        const element = arrayOfTimeBlocks[index];
-        console.log(element);
+// var timeBlockElements = document.querySelectorAll(".time-block")
+var $timeBlockElements = $(".time-block")
+console.log($timeBlockElements)
 
-    }
+
+
+var setTimeClass = function ($arrayOfTimeBlocks) {
+    //for (let index = 0; index < arrayOfTimeBlocks.length; index++) {
+    //     const element = arrayOfTimeBlocks[index];
+    //     console.log(element); }
+    $arrayOfTimeBlocks.each(function (index) {
+        console.log(this);
+        var $currentTimeBlock = $(this);
+        //var timeBlockHour = parseInt($currentTimeBlock.attr("id"));
+        var timeBlockHour = $currentTimeBlock.attr("id");
+        var currentHour = moment();
+    
+        var timeBlockMoment = moment(timeBlockHour)
+        console.log(timeBlockMoment.format("ha"))
+            if (currentHour.isSame(moment(timeBlockHour))) {
+            $currentTimeBlock.addClass("present")
+        } 
+        
+        //else if (currentHour > timeBlockHour) {
+          //  $currentTimeBlock.addClass("past")
+        //}
+    });
 };
-setTimeClass(timeBlockElements)
+setTimeClass($timeBlockElements)
 
 
+//Adds the specified class(es) to each element in the set of matched elements.
+//.addClass()
+
+//Store arbitrary data associated with the matched elements or return the value at the named data store for the first element in the set of matched elements.
+//.data()
 
